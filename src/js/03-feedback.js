b3683cf401b32ef463c2327147cca693a849c4ca
Mariia Holovko <mariia.glvk@gmail.com>
2022-10-06 19:15:28 +0300

    add tasks 2 and 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+let currentFormData = formData;
+
+formDataStorage();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onTextareaInput(event) {
+  currentFormData = {
+    ...currentFormData,
+    [event.target.name]: event.target.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentFormData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  currentFormData = formData;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentFormData));
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+  currentFormData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || formData;
+  return currentFormData;
+}
+
+function formDataStorage() {
+  const { email, message } = populateTextarea();
+  input.value = email;
+  textarea.value = message;
+}
